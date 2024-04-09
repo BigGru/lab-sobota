97afde4a695979174244dc00479e7aca6e747eab
Pawel Stanieta <pawelas000@gmail.com>
2024-04-09 20:09:29 +0200

    Część 1

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+public static void main(String[] args){
+    Point2 point = new Point2(7.0,10.0);
+    System.out.printf("wartość x: %s, wartosć y: %s",point.getX(),point.getY());
+}
+}
---
+++ Point2.java
@@ -0,0 +1,22 @@
+public class Point2 {
+private double x;
+private double y; 
+
+
+public Point2(double x, double y) {
+this.x = x;
+this.y = y;    
+}
+public double getX(){
+    return this.x;
+}
+public double getY(){
+    return this.y;
+}
+public void setX(double d){
+    this.x = d;
+}
+public void setY(double s){
+    this.y = s;
+}
+}
